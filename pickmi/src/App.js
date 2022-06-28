a37c8bbe65a8615d457ead8097a5118d6bbe05c5
Veronica Angelin Setiyo <setiyo.veronica@gmail.com>
2022-06-28 13:03:07 +0800

    added logo

--- pickmi/src/App.js
+++ pickmi/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from '../Navbar';
 import './App.css';
+import Home from './Home';
+import logo from './images/logo.png';
+import Header from './Header.js';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <>
+    // <Router>
+    //   {/* <Navbar/> */}
+    //   <Routes>
+    //     <Route path='/welcome' element={<Home/>} />
+    //   </Routes>
+    // </Router>
+    // </>
+
+    <div>
+      <h1>This is App landing page.</h1>
+      <Header/>
+      <img src={logo}/>
+      <Footer/>
     </div>
   );
 }
